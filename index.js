38f64e0e673b1fae71466d50504a2ef6aac7541c
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-06-07 21:21:26 +0300

    commit tree c56a97c8bcdc6eda55ae918a70422ff440db2ff6

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+
+    return blocks > 42 ? blocks - 42 : 42 - blocks
+    }
+    
+    function distanceFromHqInFeet(blocks) {
+    let feet=distanceFromHqInBlocks(blocks)*264
+    return feet
+    }
+    function distanceTravelledInFeet(start, destination){
+    return start<destination? (destination-start)*264: (start-destination)*264
+    }
+    function calculatesFarePrice(start, destination){
+        let distance = distanceTravelledInFeet(start, destination)
+        if(distance<= 400){
+            return 0
+        }
+        else if(400<distance && distance <2000){
+            return (distance-400)*0.02
+        }
+        else if(distance>2000 && distance<2500){
+            return 25
+        }
+        else{
+            return'cannot travel that far'
+        }
+    }
